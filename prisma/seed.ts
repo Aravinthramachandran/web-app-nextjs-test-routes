1cbd4640e31451ff2f11248d82d4bef8fae67227
Aravinth Ramachandran <aravinth.r@uniquestinfragroup.com>
2025-03-12 23:41:06 +0530

    verify aad token for private route

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,12 +3,13 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
+  await prisma.user.deleteMany({});
   const user = await prisma.user.upsert({
     where: { email: "test@test.com" },
     update: {},
     create: {
       email: "test@test.com",
-      name: "Test User",
+      name: "AR",
       password: `$2y$12$GBfcgD6XwaMferSOdYGiduw3Awuo95QAPhxFE0oNJ.Ds8qj3pzEZy`, //password
     },
   });
